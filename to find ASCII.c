153be86535ac64bea2f98a9fc61f2479fb7569e2
salavthapa <salav.thapa4@gmail.com>
2016-11-23 12:32:01 +0545

    Add files via upload

---
+++ to find ASCII.c
@@ -0,0 +1,30 @@
+/*
+student Name:Salav Thapa
+subject:Programing fundamental
+Roll No:
+Program:To find ASCII value or any charater assigned from users
+Lab Sheet No:5
+Date:2073-08-8
+*/
+
+#include<stdio.h>
+int main(){
+    char c;
+    int asciivalue;
+    printf("enter the value of character");
+    scanf("%c",&c);
+    printf("ASCII value of %c=%d",c,c);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
